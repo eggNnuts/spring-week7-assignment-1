dbf3c580b66666b82136c296180d02ac08702cbe
eggNnuts <51026414+eggNnuts@users.noreply.github.com>
2022-05-05 18:35:20 +0900

    Revert "PROJ-1_유저 생성"

--- app/src/main/java/com/codesoom/assignment/errors/ControllerExceptionHandler.java
+++ app/src/main/java/com/codesoom/assignment/errors/ControllerExceptionHandler.java
@@ -4,44 +4,30 @@ package com.codesoom.assignment.errors;
 import com.codesoom.assignment.common.CommonResponse;
 import com.codesoom.assignment.errors.user.UserEmailDuplicationException;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.validation.ConstraintViolationException;
 
 @Slf4j
 @ControllerAdvice
-public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
+public class ControllerExceptionHandler {
 
 
+    @ResponseBody
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(UserEmailDuplicationException.class)
-    public CommonResponse onUserEmailDuplicationException(RuntimeException e) {
+    public CommonResponse onUserEmailDuplicationException(RuntimeException e){
         return CommonResponse.fail(e.getMessage());
     }
 
-    @Override
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(
-            MethodArgumentNotValidException exception,
-            HttpHeaders headers,
-            HttpStatus status,
-            WebRequest request) {
-
-        BindingResult bindingResult = exception.getBindingResult();
-
-        StringBuilder errorMessageBuilder = new StringBuilder();
-        for (FieldError fieldError : bindingResult.getFieldErrors()) {
-            errorMessageBuilder.append(fieldError.getDefaultMessage());
-        }
-
-        return new ResponseEntity(CommonResponse.fail(errorMessageBuilder.toString()), headers, status);
+    @ResponseBody
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(ConstraintViolationException.class)
+    public CommonResponse onConstraintViolationException(ConstraintViolationException e) {
+        return null;
     }
 }
